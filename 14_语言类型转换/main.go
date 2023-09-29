29447cf9c46447f68a09bdbf13fa837f709ab024
linkh <421698843@qq.com>
2023-09-29 11:09:52 +0800

    finish initial version

---
+++ 14_语言类型转换/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func fun1() {
+	var sum int = 17
+	var count int = 5
+	var mean float32
+
+	mean = float32(sum) / float32(count)
+	fmt.Printf("mean 的值为: %f\n", mean)
+}
+
+func fun2() {
+	str := "123"
+	num, err := strconv.Atoi(str)
+	if err != nil {
+		fmt.Println("转换错误:", err)
+	} else {
+		fmt.Printf("字符串 '%s' 转换为整数为：%d\n", str, num)
+	}
+}
+
+// 接口类型转换
+func fun3() {
+	var i interface{} = "Hello, World"
+	str, ok := i.(string)
+	if ok {
+		fmt.Printf("'%s' is a string\n", str)
+	} else {
+		fmt.Println("conversion failed")
+	}
+}
+
+func main() {
+}
